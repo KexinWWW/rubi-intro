66e2f9ad5c6b6c47d2bd39b9ee4e8ca1150c1900
KexinWWW <kexinwang2023@u.northwestern.edu>
2023-01-24 01:09:23 +0000

    1/23 lecture

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -7,13 +7,42 @@
 # Numbers
 
 # Perform simple math with numbers
+puts 5 + 2 
+puts 5.0 / 2.0
+puts 5 * (2 + 1)
 
 # Strings
+puts "Hello, world!"
 
 # Combine strings together
+puts "Tacos are" + "delish"
+puts "Tacos" * 3
+
+#puts 3 * "Tacos"
+puts "Tacos" + 3.to_s
+#converting an integer to a string
 
 # Variables
+food = "Tacos"
+quantity = 3
+puts food * quantity
 
 # Combine strings and variables
+first_name = "Boba"
+puts "Hello, #{first_name}!"
+#will come out as "Hello, Boba!"
+greeting = "Hello, #{first_name}!"
+puts greeting
+
+puts "tacos: #{3}"
+puts "#{food}: #{quantity}"
+
 
 # String manipulation
+puts "Hello".length
+puts "Hello".reverse
+puts "Hello".upcase
+puts "Hello".downcase
+puts "Hello".swapcase
+#oder of operation
+
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,61 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+is_true = true
+is_false = is_false
+
+#puts is_true
 
 # Boolean Expressions
+puts 3 < 2
+#turns out to be "false"
 
 # If Conditional Logic
+if 3 > 2
+    puts "math works!"
+end
 
+if 3 < 2
+    puts "what???"
+end
+
+#只要有if 马上type in end
 # If/Else Conditional Logic
+if 3 > 2
+    puts "math stil works!"
+else
+    puts "what???"
+end
+
+user_password = "tacos"
+submitted_password = "password1"
+if submitted_password == user_password
+    puts "you're logged in!"
+else
+    puts "try again"
+end
+
+
 
 # Elsif Conditional Logic
+team1_score = 101
+team2_score = 101
+if team1_score > team2_score
+    puts "team1 wins!"
+elsif team2_score > team1_score
+    puts "team2 wins!"
+else
+    puts "it's a tie!"
+end
+
 
 # Combining Expressions
+temp = 81
+if temp > 60 && temp < 80
+    puts "it's so nice!"
+elsif temp>= 80 || temp <= 60
+    puts "why???"
+end
+
+# &&是and
+# || 是or
